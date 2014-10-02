d89b6a9d8be7e3869e11b65ac10b265f53d27b7a
Schneider Werner-Walter <wschneider@developmentgateway.org>
2014-10-03 02:14:01 +0300

    AMP-18104: Update module "Entityqueue" to the latest code onthe branch 7-1.x [2014-Oct-02]

--- web/sites/all/modules/contrib/entityqueue/entityqueue.api.php
+++ web/sites/all/modules/contrib/entityqueue/entityqueue.api.php
@@ -11,6 +11,23 @@
  */
 
 /**
+ * This hook is needed to allow modules to provide their own queues.
+ *
+ * If you do this, CTools will look for default queues in
+ * <modulename>.entityqueue_default.inc
+ *
+ * @see hook_entityqueue_default_queues()
+ *
+ * @return array
+ *   An associative array containing the entityqueue version.
+ */
+function hook_entityqueue_api($module = NULL, $api = NULL) {
+  if ($module == 'entityqueue' && $api == 'entityqueue_default') {
+    return array('version' => '1');
+  }
+}
+
+/**
  * This hook allows modules to provide their own queues.
  *
  * @return array
@@ -25,12 +42,15 @@ function hook_entityqueue_default_queues() {
   $queue->name = 'featured_articles';
   $queue->label = 'Featured articles';
   $queue->handler = 'simple';
+  // if multilingual
+  $queue->language = 'en';
   $queue->target_type = 'node';
   $queue->settings = array(
     'target_bundles' => array(),
     'min_size' => 0,
     'max_size' => 0,
     'subqueue_label' => '',
+    'act_as_queue' => 1,
   );
   $queues['featured_articles'] = $queue;
 
