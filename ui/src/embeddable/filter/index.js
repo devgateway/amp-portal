5aadef08692a033738211cf48b430a1edacbbf54
sdimunzio <sdimunzio@gmail.com>
2021-03-23 11:24:20 -0300

    project files

--- ui/src/embeddable/filter/index.js
+++ ui/src/embeddable/filter/index.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import {Container} from "semantic-ui-react";
 import {connect} from "react-redux";
 import {getCategories, setFilter} from "../../data/module";
-import {DropDownFilter} from '../../components/Components'
+import {DropDownFilter} from './Components'
 import './filter.scss'
 
 const Filter = ({
