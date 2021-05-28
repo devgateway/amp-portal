ea5bbcb9c5b3ab46a8f4c76ff96197809d2be502
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-05-28 14:09:15 -0300

    AMPPP-506 donor scorecard placeholder

---
+++ ui/src/charts/donorScorecard/DonorScoreCard.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import './DonorScoreCard.scss';
+export default class DonorScoreCard extends Component {
+  render() {
+    return <div className={"donor-scorecard-chart"}>Donor scorecard</div>;
+  }
+}
