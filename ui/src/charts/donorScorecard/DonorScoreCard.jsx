b4f26fe62d024f56f1988934dedf514c75ac875b
galina.kalvatcheva <galina.kalvatcheva@coreonyx.com>
2021-06-01 14:29:29 -0400

    task/AMPPP-506/donor-scorecard
    - Donor scorecard styling
    - css mixing and variables adjustments to re-use for future modules

--- ui/src/charts/donorScorecard/DonorScoreCard.jsx
+++ ui/src/charts/donorScorecard/DonorScoreCard.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './DonorScoreCard.scss';
 export default class DonorScoreCard extends Component {
   render() {
-    return <div className={"donor-scorecard-chart"}>Donor scorecard</div>;
+    return <div className={"donor-scorecard-chart"}>
+      <h3>Donor Score Card</h3>
+      <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lectus ut neque blandit ultricies.</div>
+    </div>;
   }
 }
