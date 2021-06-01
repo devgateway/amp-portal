ffe3282276154e1bc1598e58ad2688846d8bdc1c
galina.kalvatcheva <galina.kalvatcheva@coreonyx.com>
2021-06-01 15:16:07 -0400

    task/AMPPP-506/donor-scorecard
    - add data labels and container for the visualization

--- ui/src/charts/donorScorecard/DonorScoreCard.jsx
+++ ui/src/charts/donorScorecard/DonorScoreCard.jsx
@@ -5,6 +5,20 @@ export default class DonorScoreCard extends Component {
     return <div className={"donor-scorecard-chart"}>
       <h3>Donor Score Card</h3>
       <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lectus ut neque blandit ultricies.</div>
+      <div className="data-wrapper">
+        <div className="data-item">
+          <span className="label">On-Time</span>
+          <div className="chart-wrapper">chart goes here</div>
+        </div>
+        <div className="data-item">
+          <span className="label">Late</span>
+          <div className="chart-wrapper">chart goes here</div>
+        </div>
+        <div className="data-item">
+          <span className="label">No Updates</span>
+          <div className="chart-wrapper">chart goes here</div>
+        </div>
+      </div>
     </div>;
   }
 }
