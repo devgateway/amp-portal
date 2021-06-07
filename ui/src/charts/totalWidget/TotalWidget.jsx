01b614c5bdf3cc032ce2bcc887c327017d6bd433
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-06-07 16:25:31 -0300

    AMPPP-537 total widget place holder

---
+++ ui/src/charts/totalWidget/TotalWidget.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './TotalWidget.scss';
+export default class TotalWidget extends Component {
+  render() {
+    return <div className={"top-widget-chart"}>
+      <h3>TOTAL WIDGET CHART</h3>
+    </div>;
+  }
+}
