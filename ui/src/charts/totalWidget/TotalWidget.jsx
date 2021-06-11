c6de7bee89a4579023cb2bcb2e2caa538b908bd2
galina.kalvatcheva <galina.kalvatcheva@coreonyx.com>
2021-06-11 00:02:01 -0400

    task/AMPPP-537/total-widget
    -  commitments blocks

--- ui/src/charts/totalWidget/TotalWidget.jsx
+++ ui/src/charts/totalWidget/TotalWidget.jsx
@@ -3,7 +3,9 @@ import './TotalWidget.scss';
 export default class TotalWidget extends Component {
   render() {
     return <div className={"top-widget-chart"}>
-      <h3>TOTAL WIDGET CHART</h3>
+      <h3>Total Disbursements</h3>
+      <span className="indicator down"></span>
+      <span className="value">94,867,568,000</span>
     </div>;
   }
 }
